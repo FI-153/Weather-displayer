7577c29405b52eafad37f4d5c73c38bc7c4dfd87
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2022-02-01 22:26:16 +0100

    Added conditions and associated decoding operations to WeatherData

--- Weather displayer/Models/WeatherData.swift
+++ Weather displayer/Models/WeatherData.swift
@@ -13,17 +13,19 @@ struct Day: Identifiable, Decodable {
     var datetime:		String?
     var temp:			Float?
     var precip:		Float?
+	var conditions:	String?
     var description:	String?
     
-    private init(datetime: String, temp: Float, precip: Float, description: String) {
+    private init(datetime: String, temp: Float, precip: Float, conditions:	String, description: String) {
         self.datetime = 	datetime
         self.temp = 		temp
         self.precip = 	precip
+		self.conditions =	conditions
         self.description = description
     }
     
     private enum CodingKeys: String, CodingKey {
-        case datetime, temp, precip, description
+        case datetime, temp, precip, conditions, description
     }
     
     init(from decoder: Decoder) throws {
@@ -33,6 +35,7 @@ struct Day: Identifiable, Decodable {
             self.datetime = 	try container.decode(String.self, forKey: CodingKeys.datetime)
             self.temp = 		try container.decode(Float.self, forKey: CodingKeys.temp)
             self.precip = 	try container.decode(Float.self, forKey: CodingKeys.precip)
+			self.conditions =	try container.decode(String.self, forKey: CodingKeys.conditions)
             self.description = try container.decode(String.self, forKey: CodingKeys.description)
         }catch let error {
             print(error)
@@ -41,8 +44,8 @@ struct Day: Identifiable, Decodable {
     
     ///Mock data to be used during development
     static let mockData = [
-        Day(datetime: "date", temp: 8.1, precip: 0, description: "Desctiption of weather data"),
-        Day(datetime: "date2", temp: 9.5, precip: 1, description: "Desctiption of weather data2")
+		Day(datetime: "date", temp: 8.1, precip: 0, conditions: "cloudy", description: "Desctiption of weather data"),
+		Day(datetime: "date2", temp: 9.5, precip: 1, conditions: "snow", description: "Desctiption of weather data2")
     ]
 }
 
