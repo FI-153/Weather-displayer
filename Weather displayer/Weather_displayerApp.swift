596265624902c2b0b124f5079f0642fc065f7d2c
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2022-02-01 19:21:10 +0100

    Initial Commit

---
+++ Weather displayer/Weather_displayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_displayerApp.swift
+//  Weather displayer
+//
+//  Created by Federico Imberti on 01/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_displayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
