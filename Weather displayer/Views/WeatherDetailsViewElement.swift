96aa7ef07d39662cfa23d5f69de3fb654e7faa20
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2022-02-11 21:18:07 +0100

    Added WeatherDetailsViewElement to display details of the daily weather forecast

---
+++ Weather displayer/Views/WeatherDetailsViewElement.swift
@@ -0,0 +1,71 @@
+//
+//  WeatherDetailsViewElement.swift
+//  Weather displayer
+//
+//  Created by Federico Imberti on 11/02/22.
+//
+
+import SwiftUI
+
+struct WeatherDetailsViewElement: View {
+	var title:String
+	var image:String?
+	var displayedInfo:String?
+	var bigValue:String?
+	
+    var body: some View {
+		ZStack {
+			RoundedRectangle(cornerRadius: 15)
+				.fill(Color.secondary.opacity(0.3))
+			
+			VStack{
+				Text(title)
+					.font(.title3)
+					.fontWeight(.semibold)
+				
+				if
+					let image = image,
+					let displayedInfo = displayedInfo
+				{
+					Image(systemName: image)
+						.resizable()
+						.scaledToFit()
+						.frame(width: 50, height: 50)
+
+					Text(displayedInfo)
+						.font(.headline)
+					
+				} else if
+					let bigValue = bigValue
+				{
+					
+					Text(bigValue)
+						.font(.largeTitle)
+						.bold()
+						.padding()
+					
+				}
+			}
+			
+		}
+		.frame(width: 150, height: 150)
+		
+	}
+}
+
+struct WeatherDetailsViewElement_Previews: PreviewProvider {
+	static var previews: some View {
+		VStack {
+			HStack {
+				WeatherDetailsViewElement(title: "Max", bigValue: "21°C")
+				WeatherDetailsViewElement(title: "Min", bigValue: "-1°C")
+			}
+			
+			HStack {
+				WeatherDetailsViewElement(title: "Precipitation", image: "cloud.drizzle.fill", displayedInfo: "12 mm")
+				WeatherDetailsViewElement(title: "Wind Speed", image: "wind", displayedInfo: "34 KM/h")
+			}
+			
+		}
+	}
+}
