543ee70aea8b89a861e6bf05a8affe3bb0d8736f
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2022-02-17 22:08:50 +0100

    WeatherDataElementTile now only shoes the first configuration (min and max temp.)
    Deleted showSecondConfiguration

--- WatchIntegration WatchKit Extension/View/WeatherDataElementTile.swift
+++ WatchIntegration WatchKit Extension/View/WeatherDataElementTile.swift
@@ -9,32 +9,14 @@ import SwiftUI
 
 struct WeatherDataElementTile: View {
 	
-	var title:String?
-	var image:String?
-	var displayedInfo:String?
-	var bigValue:String?
+	var title:String
+	var bigValue:String
 	
 	var body: some View {
 		ZStack {
 			background
 			
-			VStack{
-				
-				if
-					let image = image,
-					let displayedInfo = displayedInfo
-				{
-					showFirstConfiguration(image, displayedInfo)
-					
-				} else if
-					let title = title,
-					let bigValue = bigValue
-				{
-					showSecondConfiguration(title, bigValue)
-					
-				}
-				
-			}
+			showSecondConfiguration(title, bigValue)
 		}
 		
 	}
@@ -45,20 +27,6 @@ extension WeatherDataElementTile {
 		RoundedRectangle(cornerRadius: 10)
 			.fill(Color.secondary.opacity(0.3))
 			.frame(width: 70, height: 70)
-	}
-}
-
-private func showFirstConfiguration(_ image:String, _ displayedInfo:String) -> some View{
-	VStack{
-		Image(systemName: image)
-			.resizable()
-			.scaledToFit()
-			.symbolRenderingMode(.multicolor)
-			.frame(height: 20)
-		
-		Text(displayedInfo)
-			.fontWeight(.semibold)
-			.padding(3)
 	}
 }
 
@@ -76,15 +44,9 @@ private func showSecondConfiguration(_ title:String, _ bigValue:String) -> some 
 struct WeatherDataElement_Previews: PreviewProvider {
 	static var previews: some View {
 		ScrollView(.vertical) {
-			VStack(spacing: 15){
-				HStack(spacing: 15) {
-					WeatherDataElementTile(title: "Max", bigValue: "10°C")
-					WeatherDataElementTile(title: "Min", bigValue: "-1°C")
-				}
-				HStack(spacing: 15) {
-					WeatherDataElementTile(title: "Precipitation", image: "cloud.drizzle.fill", displayedInfo: "12 mm")
-					WeatherDataElementTile(title: "Wind Speed", image: "wind", displayedInfo: "12 Km/h")
-				}
+			HStack(spacing: 15) {
+				WeatherDataElementTile(title: "Max", bigValue: "10°C")
+				WeatherDataElementTile(title: "Min", bigValue: "-1°C")
 			}
 		}
 		
