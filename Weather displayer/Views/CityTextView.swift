ca690782d6385725aa2ec8b50e492381b81c6872
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2023-09-30 18:12:04 +0200

    Refactoring of CityTextView

--- Weather displayer/Views/CityTextView.swift
+++ Weather displayer/Views/CityTextView.swift
@@ -5,52 +5,58 @@
 //  Created by Federico Imberti on 02/02/22.
 //
 
-import Foundation
 import SwiftUI
 
 struct CityTextView: View {
 	
 	@Binding var titleText:String
 	@Binding var isCityTextViewSelected:Bool
-	var provinceAndCountry:String
+    var provinceAndCountry:String
 
 	private let downloadDataManager = DownloadDataManager.getShared()
 
 	var body: some View {
-		VStack(alignment: .leading) {
-			Group{
-				TextField("", text: $titleText)
-					.textFieldStyle(.plain)
-					.keyboardType(.asciiCapable)
-					.font(.system(size: 35, weight: .bold))
-					.submitLabel(.go)
-					.onSubmit {
-						isCityTextViewSelected = false
-						
-						Task {
-							do {
-								try await downloadDataManager.downloadWeatherData(for: titleText)
-							} catch let error{
-								print(error.localizedDescription)
-							}
-						}
-					}
-					.onTapGesture {
-						isCityTextViewSelected = true
-					}
-				
-				Text(self.provinceAndCountry)
-					.font(.title2)
-					.fontWeight(.semibold)
-			}
-			.minimumScaleFactor(0.9)
-			.foregroundColor(.white)
-			.frame(maxWidth: .infinity, alignment: .leading)
-			.padding(.horizontal)
-			
-		}
+        VStack(alignment: .leading) {
+            cityNameView
+            provinceAndCountryView
+        }
+        .minimumScaleFactor(0.9)
+        .foregroundColor(.white)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal)
 	}
 }
+
+extension CityTextView {
+    private var cityNameView: some View {
+        TextField("", text: $titleText)
+            .textFieldStyle(.plain)
+            .keyboardType(.asciiCapable)
+            .font(.system(size: 35, weight: .bold))
+            .submitLabel(.go)
+            .onSubmit {
+                isCityTextViewSelected = false
+                
+                Task {
+                    do {
+                        try await downloadDataManager.downloadWeatherData(for: titleText)
+                    } catch let error{
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+            .onTapGesture {
+                isCityTextViewSelected = true
+            }
+    }
+    
+    private var provinceAndCountryView: some View{
+        Text(self.provinceAndCountry)
+            .font(.title2)
+            .fontWeight(.semibold)
+    }
+}
+
 
 struct CityTextView_Previews: PreviewProvider {
 	static var previews: some View {
