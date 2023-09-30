d09b96893ffa8b811bda7ae45db43a366bac7bc6
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2023-09-30 17:47:35 +0200

    Refactoring WeatherView & WeatherViewModel
    - Refactored views to computed properties to have a better understanding of the screen
    - Refactored old instructions

--- Weather displayer/Screens/WeatherView/WeatherView.swift
+++ Weather displayer/Screens/WeatherView/WeatherView.swift
@@ -8,129 +8,139 @@
 import SwiftUI
 
 struct WeatherView: View {
-	@StateObject private var vm = WeatherViewModel()
-	
-	var body: some View {
-		GeometryReader { geometry in
-			ZStack {
-				BackgroundView()
-				
-				VStack{
-					
-					Spacer()
-					
-					//MARK: - City name
-					CityTextView(
-						titleText: $vm.titleString,
-						isCityTextViewSelected: $vm.isCityTextViewFocused,
-						provinceAndCountry: vm.provinceAndCountry
-					)
-					
-					//MARK: - Highlighted weather
-					Group {
-						if
-							let conditions = 	vm.highlightedWeather.icon,
-							let temparature = 	vm.highlightedWeather.temp,
-							let description = 	vm.highlightedWeather.description
-						{
-							MainWeatherIcon(
-								imageName: 	conditions,
-								temperature: 	temparature,
-								description: 	description
-							)
-								.frame(width: 350, height: 350)
-						}
-						
-						//MARK: - Weather next days
-						ScrollView(.horizontal, showsIndicators: false){
-							HStack{
-								ForEach(vm.nextDays){ day in
-									
-									if
-										let dayOfTheWeek =	day.datetime,
-										let conditions = 	day.icon,
-										let temperature = 	day.temp,
-										let precipitation = 	day.precipProb
-									{
-										Button {
-											vm.changeHighlightedWeater(to: day)
-										} label: {
-											WeatherDayView(
-												dayOfWeek: dayOfTheWeek,
-												imageName: conditions,
-												temperature: temperature,
-												precipitaionChance: precipitation,
-												isSelected: day == vm.highlightedWeather
-											)
-										}
-									}
-									
-								}
-							}
-						}
-						.padding([.horizontal, .bottom])
+    @StateObject private var vm = WeatherViewModel()
+    
+    var body: some View {
+        ZStack {
+            BackgroundView()
+            
+            VStack{
+                Spacer()
+                cityNameView
+                Group {
+                    mainWeatherView
+                    weatherNextDaysView
+                    Spacer()
+                    buttonsView
+                    Spacer()
+                }
+                .blur(radius: vm.blurRadiusForchangingCity)
+            }
+            .blur(radius: vm.blurRadiusForLoading)
+            .disabled(vm.isUiDisabled)
+            .sheet(isPresented: $vm.isSheetShown) {
+                weatherDetailsView
+            }
+            
+#if !DEBUG
+            if vm.isLoading { loadingView }
+#endif
+        }
+    }
+    
+}
 
-						Spacer()
-						
-						//MARK: - Buttons
-						
-						HStack{
-							Group{
-								Button {
-									vm.isSheetShown = true
-								} label: {
-									Text("Show More")
-										.fontWeight(.semibold)
-								}
-								
-								Button {
-									
-								} label: {
-									Image(systemName: "location")
-								}
-							}
-							.buttonStyle(.bordered)
-							.tint(.white)
-							.font(.title)
-						}
-						
-						Spacer()
-						
-					}
-					.blur(radius: vm.blurRadiusForchangingCity)
-					.sheet(isPresented: $vm.isSheetShown) {
-						ZStack{
-							BackgroundView()
-							WeatherDetailsView(vm: WeatherDetailsViewModel(isSheetShown: $vm.isSheetShown, day: vm.highlightedWeather))
-						}
-					}
-				}
-#if !DEBUG
-				.blur(radius: vm.blurRadiusForLoading)
-				.disabled(vm.isUiDisabled)
-#endif
-				
-#if !DEBUG
-				if vm.isLoading {
-					ZStack{
-						BackgroundView()
-						ProgressView()
-					}
-				}
-#endif
-			}
-		}
-		
-	}
+extension WeatherView {
+
+    private var cityNameView: some View{
+        CityTextView(
+            titleText: $vm.titleString,
+            isCityTextViewSelected: $vm.isCityTextViewFocused,
+            provinceAndCountry: vm.provinceAndCountry
+        )
+    }
+    
+    private var mainWeatherView: some View{
+        if
+            let conditions = vm.highlightedWeather.icon,
+            let temparature = vm.highlightedWeather.temp,
+            let description = vm.highlightedWeather.description
+        {
+            return MainWeatherIcon(
+                imageName: conditions,
+                temperature: temparature,
+                description: description
+            )
+            .frame(width: 350, height: 350)
+        } else {
+            return MainWeatherIcon(
+                imageName: "conditions",
+                temperature: 0.0,
+                description: "description"
+            )
+            .frame(width: 350, height: 350)
+        }
+    }
+    
+    private var weatherNextDaysView: some View{
+        ScrollView(.horizontal, showsIndicators: false){
+            HStack{
+                ForEach(vm.nextDays){ day in
+                    if
+                        let dayOfTheWeek = day.datetime,
+                        let conditions = day.icon,
+                        let temperature = day.temp,
+                        let precipitation = day.precipProb
+                    {
+                        Button {
+                            vm.changeHighlightedWeater(to: day)
+                        } label: {
+                            WeatherDayView(
+                                dayOfWeek: dayOfTheWeek,
+                                imageName: conditions,
+                                temperature: temperature,
+                                precipitaionChance: precipitation,
+                                isSelected: day == vm.highlightedWeather
+                            )
+                        }
+                    }
+                }
+            }
+        }
+        .padding([.horizontal, .bottom])
+    }
+    
+    private var buttonsView: some View{
+        HStack{
+            Group{
+                Button {
+                    vm.isSheetShown = true
+                } label: {
+                    Text("Show More")
+                        .fontWeight(.semibold)
+                }
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "location")
+                }
+            }
+            .buttonStyle(.bordered)
+            .tint(.white)
+            .font(.title)
+        }
+    }
+    
+    private var weatherDetailsView: some View{
+        ZStack{
+            BackgroundView()
+            WeatherDetailsView(vm: WeatherDetailsViewModel(isSheetShown: $vm.isSheetShown, day: vm.highlightedWeather))
+        }
+    }
+    
+    private var loadingView: some View{
+        ZStack{
+            BackgroundView()
+            ProgressView()
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		Group {
-			WeatherView()
-			
-			WeatherView()
-				.preferredColorScheme(.dark)
-		}
-	}
+    static var previews: some View {
+        Group {
+            WeatherView()
+        }
+    }
 }
--- Weather displayer/Screens/WeatherView/WeatherViewModel.swift
+++ Weather displayer/Screens/WeatherView/WeatherViewModel.swift
@@ -5,36 +5,35 @@
 //  Created by Federico Imberti on 01/02/22.
 //
 
-import UIKit
 import Combine
 import SwiftUI
 
 class WeatherViewModel: ObservableObject {
 	
-	@Published var titleString:String = 		WeatherData.mockData.cityname!
-	@Published var provinceAndCountry:String =	WeatherData.mockData.provinceAndCountry!
-	@Published var highlightedWeather:Day = 	Day.mockData.first!
-	@Published var nextDays:[Day] = 			Day.mockData
-	@Published var isLoading:Bool = 			true
+	@Published var titleString:String = WeatherData.mockData.cityname!
+	@Published var provinceAndCountry:String = WeatherData.mockData.provinceAndCountry!
+	@Published var highlightedWeather:Day = Day.mockData.first!
+	@Published var nextDays:[Day] = Day.mockData
+	@Published var isLoading:Bool = true
 	@Published var isCityTextViewFocused:Bool =	false
-	@Published var isSheetShown:Bool =		false
+	@Published var isSheetShown:Bool = false
 	
 	init(){
 		addSubscriberToWeatherDataArray()
 		addSubscriberToIsLoading()
 	}
 	
-	private let downloadDataManager = 	DownloadDataManager.getShared()
-	private var cancellables = 			Set<AnyCancellable>()
+	private let downloadDataManager = DownloadDataManager.getShared()
+	private var cancellables = 	Set<AnyCancellable>()
 	func addSubscriberToWeatherDataArray(){
 		downloadDataManager.$downloadedData.sink { [weak self] receivedWeather in
 			
 			guard let self = self else { return }
 			
-			self.titleString = 		receivedWeather.cityname!
-			self.provinceAndCountry = 	receivedWeather.provinceAndCountry!
-			self.highlightedWeather =		receivedWeather.days!.first!
-			self.nextDays = 			receivedWeather.days!
+			self.titleString = receivedWeather.cityname!
+			self.provinceAndCountry = receivedWeather.provinceAndCountry!
+			self.highlightedWeather = receivedWeather.days!.first!
+			self.nextDays = receivedWeather.days!
 			
 		}
 		.store(in: &cancellables)
@@ -66,7 +65,7 @@ class WeatherViewModel: ObservableObject {
 	}
 	
 	var isUiDisabled: Bool {
-		isLoading ? true : false
+		isLoading
 	}
 	
 }
