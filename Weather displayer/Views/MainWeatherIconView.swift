a227e79e987e2c0df8ddd91e6cfd312ee5547cb0
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2023-10-01 09:40:11 +0200

    Refactoring of MainWeatherIcon

--- Weather displayer/Views/MainWeatherIconView.swift
+++ Weather displayer/Views/MainWeatherIconView.swift
@@ -14,27 +14,50 @@ struct MainWeatherIcon: View {
 	var description:	String
 	
 	var body: some View {
-		VStack(spacing: 8){
-			Image(systemName: self.imageName)
-				.renderingMode(.original)
-				.resizable()
-				.scaledToFit()
-				.frame(width: 150, height: 150)
-				.shadow(radius: 10)
-				.padding(.bottom)
-			
-			Text(String(self.temperature) + "°C")
-				.font(.largeTitle)
-				.fontWeight(.bold)
-				.foregroundColor(.white)
-			
-			Text("\"" + description + "\"")
-				.font(.title2)
-				.fontWeight(.semibold)
-				.minimumScaleFactor(0.7)
-				.foregroundColor(.secondary)
-				.frame(height: 100)
+		VStack(spacing: 0){
+			mainImageView
+			temperatureView
+            descriptionView
 		}
 		.padding(.horizontal)
 	}
 }
+
+extension MainWeatherIcon {
+    private var mainImageView: some View {
+        Image(systemName: self.imageName)
+            .renderingMode(.original)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 150, height: 150)
+            .shadow(radius: 10)
+    }
+    
+    private var temperatureView: some View{
+        Text(String(self.temperature) + "°C")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+    }
+    
+    private var descriptionView: some View{
+        Text("\"" + description + "\"")
+            .font(.title2)
+            .fontWeight(.semibold)
+            .italic()
+            .minimumScaleFactor(0.7)
+            .foregroundColor(.secondary)
+            .frame(height: 100)
+    }
+}
+
+
+struct MainWeatherIcon_Previews: PreviewProvider {
+    static var previews: some View{
+        ZStack {
+            BackgroundView()
+            MainWeatherIcon(imageName: "cloud.sun.fill", temperature: 12, description: "Fine and dandy weather description")
+        }
+    }
+}
+
